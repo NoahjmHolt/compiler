0a53602225600ccf97d64872a3fa2c4b6b394855
Noah Holt <143626243+NoahjmHolt@users.noreply.github.com>
2024-01-28 11:29:34 -0700

    Givens to start
    Put the given main file from class into my main

--- hw1part1/hw1part1/src/Main.java
+++ hw1part1/hw1part1/src/Main.java
@@ -1,15 +1,51 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fa22hw1;
+import ADT.*;
+/**
+ *
+ * @author nholt
+ */
+/********************** FREE BONUS FUNCTION
+ String pad(String input, int len, boolean left) {
+ while (input.length() < len){
+ if (left)
+ input = " " +input ;
+ else
+ input = input + " ";
+ }
+ return input;
+ }
+ */
+public class main {
+    /**
+     * @param args the command line arguments
+     */
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+// Create the table
+        System.out.println("Noah Holt CS4100 Homework 1, Spring
+                2024");
+                ReserveTable reserve = new ReserveTable(25);
+// Add to the table
+        reserve.Add("cat", 15);
+        reserve.Add("APPLE", 11);
+        reserve.Add("Dog", 5);
+        reserve.Add("DOnE", 21);
+        reserve.Add("Over", 8);
+// Search the table
+        System.out.println("The Code for 'over' is "+reserve.LookupName("over"));
+        System.out.println("The Code for 'DOG' is "+reserve.LookupName("DOG"));
+        System.out.println("The Code for 'Cat' is "+reserve.LookupName("Cat"));
+        System.out.println("The Code for 'gone' is "+reserve.LookupName("gone"));
+        System.out.println();
+        System.out.println("The Name for 11 is "+reserve.LookupCode(11));
+        System.out.println("The Name for 5 is "+reserve.LookupCode(5));
+        System.out.println("The Name for 8 is "+reserve.LookupCode(8));
+        System.out.println("The Name for 28 is "+reserve.LookupCode(28));
+// Print table to file
+        System.out.println("Saving Printed Table to "+args[0]);
+        reserve.PrintReserveTable(args[0]);
     }
-}

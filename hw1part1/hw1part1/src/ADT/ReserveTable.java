4b0821d628d4b62ebbeaf06b79467bfdb94fc249
Noah Holt <143626243+NoahjmHolt@users.noreply.github.com>
2024-01-28 17:15:51 -0700

    bit of fiddling
    did a bit of fittleing and adjusting but still waiting for a response from my teacher to see how he wants it changed
    
    Also need to see how the jar file things happen

--- hw1part1/hw1part1/src/ADT/ReserveTable.java
+++ hw1part1/hw1part1/src/ADT/ReserveTable.java
@@ -1,5 +1,6 @@
 package ADT;
 import java.lang.String;
+import java.util.ArrayList;
 import java.util.List;
 import java.io.File;
 import java.io.FileWriter;
@@ -7,8 +8,8 @@ import java.io.IOException;
 
 public class ReserveTable {
 
-    private List<String> stringList;
-    private List<Integer> codeList;
+    private ArrayList<String> stringList = new ArrayList<String>();
+    private ArrayList<Integer> codeList = new ArrayList<Integer>();
 
     public ReserveTable(int maxSize)
     {
@@ -18,16 +19,16 @@ public class ReserveTable {
     public int Add(String name, int code)
     {
 
-        stringList.add(name);
+        stringList.add(name.toLowerCase());
         codeList.add(code);
 
-        return stringList.indexOf(name);
+        return stringList.indexOf(name.toLowerCase());
     } // end add
 
     public int LookupName(String name)
     {
         // find the name
-        int found = stringList.indexOf(name);
+        int found = stringList.indexOf(name.toLowerCase());
         //if found return code associated
         if (found != -1) {
             return codeList.get(found);
@@ -51,7 +52,14 @@ public class ReserveTable {
     public void PrintReserveTable(String filename)
     {
 
-     
+        try {
+            FileWriter myWriter = new FileWriter(filename);
+            myWriter.write("Files in Java might be tricky, but it is fun enough!");
+            myWriter.close();
+            System.out.println("Successfully wrote to the file.");
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+        }
 
     } // end print table
 

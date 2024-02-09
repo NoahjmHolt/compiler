c119371821ca243da058d396b44bf1e7be5739a0
Noah Holt <143626243+NoahjmHolt@users.noreply.github.com>
2024-02-09 13:38:40 -0700

    Quad table
    Added comments for each method
    
    Also filled out most of the functions as they were pretty simple.
    
    Just need to print leftover

--- holt_hw1/src/ADT/QuadTable.java
+++ holt_hw1/src/ADT/QuadTable.java
@@ -2,35 +2,65 @@ package ADT;
 
 public class QuadTable {
 
+    int tableSize;
+    int nextUp;
+
+    /*
+        initialize vars and get things ready
+     */
     public QuadTable(int maxSize){
-        int[][] quadTable = new int[maxSize][4];
+        tableSize = maxSize;
+        nextUp = 0;
     } // general method
 
+    int[][] quadTable = new int[tableSize][4];
+
+    /*
+        returns the index of next empty slot in the table
+     */
     public int NextQuad(){
 
+        return nextUp;
 
-        return 0;
     } // next quad
 
+    /*
+        add new quad and increments next up
+     */
     public void AddQuad(int opcode, int op1, int op2, int op3){
 
+        quadTable[nextUp][0] = opcode;
+        quadTable[nextUp][1] = op1;
+        quadTable[nextUp][2] = op2;
+        quadTable[nextUp][3] = op3;
 
+        nextUp = nextUp +1;
 
     } // add quad
 
+    /*
+        using index, return the quad
+     */
     public int[] GetQuad(int index){
 
-        int[] placeHolder= {1, 2, 3};
-
-        return placeHolder;
+        return quadTable[index];
 
     } // Get Quad
 
+    /*
+        update op 3 of specified index
+     */
     public void UpdateJump(int index, int op3){
 
+        quadTable[index][3] = op3;
 
     } // Update Jump
 
+    /*
+        print out quad table
+        only bits in use
+        Format provided in example file
+     */
     public void PrintQuadTable(String fileName){
 
 

243f5d6abc37e72f67c6e3d9b227557d045c248a
Noah Holt <143626243+NoahjmHolt@users.noreply.github.com>
2024-02-09 15:02:56 -0700

    Think Im Done
    think I am all set, just need to work out some testing and call it a day

--- holt_hw1/src/ADT/SymbolTable.java
+++ holt_hw1/src/ADT/SymbolTable.java
@@ -106,68 +106,89 @@ public class SymbolTable {
 
     /*
         Symbol at index
+        symbol is [index][0]
      */
     public String GetSymbol(int index){
 
-        return "placeHolder";
+        return symbolTable[index][0];
 
     } // Get Symbol
 
     /*
         Usage at index
+        usage is [index][1]
      */
     public char GetUsage(int index){
 
-        return 'c';
+        return (symbolTable[index][1]).charAt(0);
 
     } // Get Usage
 
     /*
         Type at index
+        type is [index][2]
      */
     public char GetDataType(int index){
 
-        return 'c';
+        return (symbolTable[index][2]).charAt(0);
 
     } // Get Data Type
 
     /*
         String at index
+        string is [index][3]
      */
     public String GetString(int index){
 
-        return "Place Holdre";
+        return symbolTable[index][3];
 
     } // Get String
 
     /*
         int at index
+        int is [index][3]
+        cast back to int (stored as string)
      */
     public int GetInteger(int index){
 
-        return 0;
+        return Integer.parseInt(symbolTable[index][3]);
 
     } // get int
 
     /*
         float at index
+        float is [index][3]
+        Cast back to float (stored as string)
      */
     public double GetFloat(int index){
 
-        return 0;
+        return Double.parseDouble(symbolTable[index][3]);
 
     } // get float
 
     /*
         Change values if needed
+        Table goes: Name, Use, Type, Value
      */
     public void UpdateSymbol(int index, char usage, int value){
+
+        symbolTable[index][1] = String.valueOf(usage);
+        symbolTable[index][2] = String.valueOf('I');
+        symbolTable[index][3] = Integer.toString(value);
 
     }
     public void UpdateSymbol(int index, char usage, double value){
 
+        symbolTable[index][1] = String.valueOf(usage);
+        symbolTable[index][2] = String.valueOf('F');
+        symbolTable[index][3] = Double.toString(value);
+
     }
     public void UpdateSymbol(int index, char usage, String value){
+
+        symbolTable[index][1] = String.valueOf(usage);
+        symbolTable[index][2] = String.valueOf('S');
+        symbolTable[index][3] = value;
 
     }
     // end all Update Symbols
@@ -179,7 +200,23 @@ public class SymbolTable {
      */
     public void PrintSymbolTable(String fileName){
 
+        try {
+            FileWriter myWriter = new FileWriter(fileName);
 
+            myWriter.write(String.format("| %6s | %25s | %5s | %6s | %30s |%n", "Index", "Name", "Use", "Type", "Value"));
+
+            //print per element
+            for (int i = 0; i < nextUp; i++) {
+                myWriter.write(String.format("| %6s | %25s | %5s | %6s | %30s |%n", i, symbolTable[i][0], symbolTable[i][1], symbolTable[i][2], symbolTable[i][3]));
+            }
+
+            myWriter.close();
+
+            System.out.println("Successfully wrote to the file.");
+
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+        }
 
     } // Print Table
 

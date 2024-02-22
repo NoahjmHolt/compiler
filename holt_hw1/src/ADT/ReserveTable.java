44b21d363d36fde16df989908df8e5522c7f3d63
Noah Holt <143626243+NoahjmHolt@users.noreply.github.com>
2024-02-22 14:11:06 -0700

    Added Reserve Table
    added reserve table from part one to the new part so that I can do the next part.
    
    This is an iterative project and I did not understand that when begining

---
+++ holt_hw1/src/ADT/ReserveTable.java
@@ -0,0 +1,76 @@
+package ADT;
+import java.lang.String;
+import java.util.ArrayList;
+import java.util.List;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class ReserveTable {
+
+    private ArrayList<String> stringList = new ArrayList<String>();
+    private ArrayList<Integer> codeList = new ArrayList<Integer>();
+
+    public ReserveTable(int maxSize)
+    {
+
+    }
+
+    public int Add(String name, int code)
+    {
+
+        stringList.add(name.toLowerCase());
+        codeList.add(code);
+
+        return stringList.indexOf(name.toLowerCase());
+    } // end add
+
+    public int LookupName(String name)
+    {
+        // find the name
+        int found = stringList.indexOf(name.toLowerCase());
+        //if found return code associated
+        if (found != -1) {
+            return codeList.get(found);
+        }
+
+        return -1; //not found return -1
+    } // end name look up
+
+    public String LookupCode(int code)
+    {
+        // find the code
+        int found = codeList.indexOf(code);
+        // if found return name associated
+        if (found != -1){
+            return stringList.get(found);
+        }
+
+        return ""; //not found return nothing
+    } // end code look up
+
+    public void PrintReserveTable(String filename)
+    {
+
+        try {
+            FileWriter myWriter = new FileWriter(filename);
+
+            myWriter.write(String.format("| %-6s | %-10s | %5s |%n", "Index", "Name", "Code"));
+
+            //print per element
+            int listSize = stringList.size();
+            for (int i = 0; i < listSize; i++) {
+                myWriter.write(String.format("| %-6d | %-10s | %5d |%n", i, stringList.get(i), codeList.get(i)));
+            }
+
+            myWriter.close();
+
+            System.out.println("Successfully wrote to the file.");
+
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+        }
+
+    } // end print table
+
+} // end reserve table class

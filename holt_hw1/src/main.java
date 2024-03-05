90c4edafeeb31e680ca44dd93f8be5e102e9eedd
Noah Holt <143626243+NoahjmHolt@users.noreply.github.com>
2024-03-04 18:05:24 -0700

    new updated main
    changed main to match and work on new part of this assignment.
    
    Also created the new class for this section.

--- holt_hw1/src/main.java
+++ holt_hw1/src/main.java
@@ -1,47 +1,32 @@
-import ADT.*;
+//import ADT.Lexical;
 
+//import ADT.LexicalReserve;
+import ADT.*;
+//import ADT.Lexical;
 /**
  *
- * @author nholt
- *
- * This part of the project has 3 parts
- *
- * 1) Create Quad table and fill in with needed ops (see provided func)
- *
- * 2) Populate symbol table as needed
- *
- * 3) Run/interpret with provided algorithm
- *      prints answers
- *      create reserve table
- *      TRACE displays mnemonics
- *
+ * @author nholt Spring 2024
  */
 public class main {
-
     public static void main(String[] args) {
-        // Expects 6 command-line parameters for filenames,
-        //     see arg[0] through arg[5] below
-        Interpreter interp = new Interpreter();
-        SymbolTable st;
-        QuadTable qt;
-
-        // interpretation FACTORIAL
-        st = new SymbolTable(20);     //Create an empty SymbolTable
-        qt = new QuadTable(20);       //Create an empty QuadTable
-        System.out.println("This program expects command-line parameters for filenames in this order:");
-        System.out.println("traceFactorial SymbolFactorial QuadFactorial traceSum SymbolSum QuadSum");
-        interp.initializeFactorialTest(st,qt);  //Set up for FACTORIAL
-        interp.InterpretQuads(qt, st, true, args[0]);
-        st.PrintSymbolTable(args[1]);
-        qt.PrintQuadTable(args[2]);
-
-        // interpretation SUMMATION
-        st = new SymbolTable(20);     //Create an empty SymbolTable
-        qt = new QuadTable(20);       //Create an empty QuadTable
-        interp.initializeSummationTest(st,qt);  //Set up for SUMMATION
-        interp.InterpretQuads(qt, st, true, args[3]);
-        st.PrintSymbolTable(args[4]);
-        qt.PrintQuadTable(args[5]);
+        String inFileAndPath = args[0];
+        String outFileAndPath = args[1];
+        System.out.println("Lexical for " + inFileAndPath);
+        boolean traceOn = true;
+// Create a symbol table to store appropriate ident, number,string
+// symbols found NO RESERVE WORDS GO IN THE SYMBOL TABLE!
+        SymbolTable symbolList;
+        symbolList = new SymbolTable(150);
+        Lexical myLexer = new Lexical(inFileAndPath, symbolList, traceOn);
+        Lexical.token currToken;
+        currToken = myLexer.GetNextToken();
+        while (currToken != null) {
+            System.out.println("\t" + currToken.mnemonic + " | \t" +
+                    String.format("%04d", currToken.code)
+                    + " | \t" + currToken.lexeme);
+            currToken = myLexer.GetNextToken();
+        }
+        symbolList.PrintSymbolTable(outFileAndPath);
+        System.out.println("Done.");
     }
-
 }

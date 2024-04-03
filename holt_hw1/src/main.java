6014dafb4e2a6afed9e3853074648fe0a29cd861
Noah Holt <143626243+NoahjmHolt@users.noreply.github.com>
2024-04-03 16:11:44 -0600

    Who I am
    added name and identifier to output for final formatting as specified by assignment

--- holt_hw1/src/main.java
+++ holt_hw1/src/main.java
@@ -11,7 +11,7 @@ public class main {
     public static void main(String[] args) {
         String filePath = args[0];
         boolean traceon = true;
-        System.out.println("Student Name, Last 4 of student number, CS4100/5100, SPRING 2024");
+        System.out.println("noah holt, 0735, CS4100/5100, SPRING 2024");
         System.out.println("INPUT FILE TO PROCESS IS: "+filePath);
 
         Syntactic parser = new Syntactic(filePath, traceon);
